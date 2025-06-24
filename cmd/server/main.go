dbd2a10c35688af861cd8ffc9a75b2a84abadddc
icsitgithub <risky@icstravelgroup.com>
2025-06-24 22:18:43 +0800

    Initial commit: Set up Go module and basic structure

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	// You'll import your internal packages using the full module path:
+	// "github.com/yourusername/my-web-app/internal/models"
+)
+
+func main() {
+	fmt.Println("Hello from my local Go web app!")
+}
---
+++ internal/user/calculate.go
@@ -0,0 +1,9 @@
+package user
+
+import (
+	"fmt"
+)
+
+func Add(a, b int) {
+	fmt.Println(a + b)
+}
